402d995331b50245c99f8fd62eb27bec0a8314a1
Philip Krück <philip.krueck@apploft.de>
2020-06-29 15:22:24 +0200

    initial commit

---
+++ src/test/java/de/pomc/yearbook/YearbookApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pomc.yearbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
