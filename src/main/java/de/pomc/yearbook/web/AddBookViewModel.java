3556e28d268e8bec2a112afd48876e2f88071da8
Philip Krück <philip.krueck@apploft.de>
2020-07-20 20:22:15 +0200

    #controller created add book page and implemented matching controller

---
+++ src/main/java/de/pomc/yearbook/web/AddBookViewModel.java
@@ -0,0 +1,15 @@
+package de.pomc.yearbook.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AddBookViewModel {
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Getter
+    @Setter
+    private String description;
+}
