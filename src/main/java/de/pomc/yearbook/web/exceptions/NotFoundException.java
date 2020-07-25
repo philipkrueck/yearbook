326261889affd5c3a6676137d51c357bf51ec22c
Philip Krück <philip.krueck@apploft.de>
2020-07-25 11:20:23 +0200

    #errorHandling added error handling to application (403 and 404 HTTP Status Code handling added).

---
+++ src/main/java/de/pomc/yearbook/web/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package de.pomc.yearbook.web.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {}
--- src/main/java/de/pomc/yearbook/web/questionnaire/QuestionnaireEditController.java
+++ src/main/java/de/pomc/yearbook/web/questionnaire/QuestionnaireEditController.java
@@ -1,5 +1,6 @@
 package de.pomc.yearbook.web.questionnaire;
 
+import de.pomc.yearbook.web.exceptions.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,15 +11,28 @@ public class QuestionnaireEditController {
 
     @GetMapping
     public String showEditQuestionnaireView(Model model, @PathVariable("id") Long id) {
+        if (id == 1234) { // TODO: actually look up the questionnaire in the db
+            throw new NotFoundException();
+        }
+
         model.addAttribute("questionnaireViewModel", new QuestionnaireViewModel());
         return "pages/questionnaire/edit";
     }
 
     @GetMapping("/back")
-    public String goBackToQuestionnaireView(@PathVariable("id") Long id) { return "redirect:/questionnaire/{id}"; }
+    public String goBackToQuestionnaireView(@PathVariable("id") Long id) {
+        if (id == 1234) { // TODO: actually look up the questionnaire in the db
+            throw new NotFoundException();
+        }
+        return "redirect:/questionnaire/{id}";
+    }
 
     @PostMapping("/update")
     public String updateQuestionnaire(@PathVariable("id") Long id, @ModelAttribute QuestionnaireViewModel questionnaireViewModel) {
+        if (id == 1234) { // TODO: actually look up the questionnaire in the db
+            throw new NotFoundException();
+        }
+
         System.out.println("new question 1: " + questionnaireViewModel.getQuestionOne());
         System.out.println("new question 2: " + questionnaireViewModel.getQuestionTwo());
         System.out.println("new question 3: " + questionnaireViewModel.getQuestionThree());
