f2f04f5a5389fe5d9defd342e37950b86125535b
Philip Krück <philip.krueck@apploft.de>
2020-08-18 20:06:45 +0200

    [security] setup basic UserDetailsService without password encoder

--- src/main/java/de/pomc/yearbook/WebSecurityConfig.java
+++ src/main/java/de/pomc/yearbook/WebSecurityConfig.java
@@ -1,17 +1,35 @@
 package de.pomc.yearbook;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails philip = User.builder()
+                .username("philip")
+                .password("123456")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(philip);
     }
 }
