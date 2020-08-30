ee2cc62eeca4d900aa9d6fc9d1886de27d599669
ReviloR <oliver@buettner-hh.de>
2020-08-30 15:49:34 +0200

    added form-validation for signup [no UI]

--- src/main/java/de/pomc/yearbook/web/NewAccountForm.java
+++ src/main/java/de/pomc/yearbook/web/NewAccountForm.java
@@ -5,13 +5,28 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 public class NewAccountForm {
 
+    @NotEmpty(message = "Feld darf nicht leer sein.")
     private String firstName;
+    @NotEmpty(message = "Feld darf nicht leer sein.")
     private String lastName;
+    @Email(message = "Bitte geben Sie eine Email-Adresse ein.")
     private String email;
+    @Pattern(
+            regexp = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,40})",
+            message = "Das Passwort muss zwischen 8 und 40 Zeichen lang sein, " +
+                    "mindestens eine Zahl, " +
+                    "einen Kleinbuchstabend, " +
+                    "einen Großbuchstabend und " +
+                    "ein Sonderzeichen enthalten."
+    )
     private String password;
 
     public NewAccountForm() {
