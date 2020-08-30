d3aa978a9b9097f50fe70f2b6fa52415a79b29cf
ReviloR <oliver@buettner-hh.de>
2020-08-30 16:39:33 +0200

    added form-validation for profil edit [no UI]

--- src/main/java/de/pomc/yearbook/web/profile/ProfileController.java
+++ src/main/java/de/pomc/yearbook/web/profile/ProfileController.java
@@ -7,10 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/profile")
@@ -35,10 +38,17 @@ public class ProfileController {
     }
 
     @PostMapping("/edit")
-    public String editProfile(@ModelAttribute("userForm") UserForm userForm) {
+    public String editProfile(@ModelAttribute("userForm") @Valid  UserForm userForm, BindingResult bindingResult) {
 
-        // ToDo: Validate userForm
+        if(bindingResult.hasErrors()){
+            return "redirect:/profile";
+        }
+
         User user = userService.findCurrentUser();
+
+        if(userService.findUserByEmail(userForm.getEmail()) != null && !user.getEmail().equals(userForm.getEmail())){
+            return "redirect:/profile?EmailExists";
+        }
 
         UserFormConverter.update(user, userForm);
 
--- src/main/java/de/pomc/yearbook/web/profile/UserForm.java
+++ src/main/java/de/pomc/yearbook/web/profile/UserForm.java
@@ -3,22 +3,32 @@ package de.pomc.yearbook.web.profile;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class UserForm {
 
+    @NotEmpty(message = "Feld darf nicht leer sein.")
     private String firstName;
 
+    @NotEmpty(message = "Feld darf nicht leer sein.")
     private String lastName;
 
+    @Email(message = "Bitte geben Sie eine Email-Adresse ein.")
     private String email;
 
+    //@Pattern(regexp = "^@")
     private String twitterHandle;
 
     private String location;
 
+    @URL(message = "Bitte geben Sie eine gültige URL ein.")
     private String website;
 
     private String bio;
