1e8671f4d5abea27d712b3b31e19cb2a5ddd6a13
ReviloR <oliver@buettner-hh.de>
2020-08-30 18:06:43 +0200

    added form-validation for book creation [no UI]

--- src/main/java/de/pomc/yearbook/web/book/BookForm.java
+++ src/main/java/de/pomc/yearbook/web/book/BookForm.java
@@ -5,11 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @Getter
 @Setter
 public class BookForm {
+
+    @Size(min = 5, max = 50, message = "Der Name muss zwischen 5 und 50 Zeichen lang sein.")
+    @NotEmpty(message = "Feld darf nicht leer sein.")
     private String name;
+
+    @Size(min = 5, max = 100, message = "Die Beschreibung muss zwischen 5 und 100 Zeichen lang sein.")
+    @NotEmpty(message = "Feld darf nicht leer sein.")
     private String description;
 
     public BookForm() {
