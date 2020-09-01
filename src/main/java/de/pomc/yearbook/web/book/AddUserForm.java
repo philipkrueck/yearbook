7212cf69df8e56194ee8d276cc8db8df0f772f70
ReviloR <oliver@buettner-hh.de>
2020-09-01 21:17:50 +0200

    added form-validation for book creation - add participants [no UI]

--- src/main/java/de/pomc/yearbook/web/book/AddUserForm.java
+++ src/main/java/de/pomc/yearbook/web/book/AddUserForm.java
@@ -3,9 +3,12 @@ package de.pomc.yearbook.web.book;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+
 public class AddUserForm {
 
     @Getter
     @Setter
+    @Email(message = "Bitte geben Sie eine Email-Adresse an")
     private String email = "";
 }
