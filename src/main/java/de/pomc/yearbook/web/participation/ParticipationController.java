291f6cc493e3f3bdf588a504523274fe3fcd5fd5
Philip Krück <philip.krueck@apploft.de>
2020-09-05 12:59:14 +0200

    added form validatio for comment section on participation

--- src/main/java/de/pomc/yearbook/web/participation/ParticipationController.java
+++ src/main/java/de/pomc/yearbook/web/participation/ParticipationController.java
@@ -1,6 +1,7 @@
 package de.pomc.yearbook.web.participation;
 
 import de.pomc.yearbook.SampleData;
+import de.pomc.yearbook.book.Book;
 import de.pomc.yearbook.book.BookService;
 import de.pomc.yearbook.participation.Comment;
 import de.pomc.yearbook.participation.Participation;
@@ -33,20 +34,23 @@ public class ParticipationController {
         return participation.getParticipant().getId().equals(user.getId());
     }
 
+    @ModelAttribute("book")
+    public Book getBook() {
+        return SampleData.getBooks().get(0);
+    }
+
+   @ModelAttribute("participation")
+   public Participation getParticipation(@PathVariable("id") Long id) {
+        return participationService.getParticipationWithID(id);
+   }
+
     @GetMapping
     public String showParticipationView(Model model, @PathVariable("id") Long id) {
-        Participation participation = participationService.getParticipationWithID(id);
-
         // ToDo: make sure that user is either part of the book of this participation or the book is published
-
-        model.addAttribute("participation", participation);
-        // ToDo: use participation.getBook() in future
-        model.addAttribute("book", SampleData.getBooks().get(0));
-
         // ToDo: add commentForm only if user is part of the book
-        model.addAttribute("commentForm", new CommentForm());
 
-        model.addAttribute("showEditButton", currentUserIsParticipant(participation));
+        model.addAttribute("commentForm", new CommentForm());
+        model.addAttribute("showEditButton", currentUserIsParticipant(getParticipation(id)));
 
         return "pages/participation/show";
     }
@@ -55,9 +59,9 @@ public class ParticipationController {
     @PostMapping("/addComment")
     public String addComment(@PathVariable("id") Long id, @ModelAttribute("commentForm") @Valid CommentForm commentForm, BindingResult bindingResult) {
 
-       // if(bindingResult.hasErrors()){
-       //     return "pages/participation/show";
-        // }
+        if(bindingResult.hasErrors()){
+            return "pages/participation/show";
+        }
 
         // Todo: check that current user is participant of book of this participation
 
