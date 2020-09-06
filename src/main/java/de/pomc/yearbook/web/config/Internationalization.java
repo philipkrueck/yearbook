25bc637bb642e186113080e8e54e65380a00428a
MTolla <malte.tolla@myhsba.de>
2020-09-06 21:24:03 +0200

    clean-up

--- src/main/java/de/pomc/yearbook/web/config/Internationalization.java
+++ src/main/java/de/pomc/yearbook/web/config/Internationalization.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
 import java.util.Locale;
 
 @Configuration
