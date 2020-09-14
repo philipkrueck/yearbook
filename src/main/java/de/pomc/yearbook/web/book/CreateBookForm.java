1c5d0db960f5be74dc9c77b4ef4740fa48553b2a
Philip Krück <philip.krueck@apploft.de>
2020-09-14 21:24:01 +0200

    added CreateBookForm

--- src/main/java/de/pomc/yearbook/web/book/CreateBookForm.java
+++ src/main/java/de/pomc/yearbook/web/book/CreateBookForm.java
@@ -1,7 +1,10 @@
 package de.pomc.yearbook.web.book;
 
+import lombok.Getter;
+
 public class CreateBookForm extends BookForm {
 
+    @Getter
     private byte[] image;
 
     public CreateBookForm(String name, String description, boolean published, byte[] image) {
